3fe45bda13650f006d9193efedeecde80e755d5f
jValdron <jason@valdron.ca>
2013-04-29 10:56:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 try {
-  module.exports = require('./build/default/vips');
-} catch(e) {
-  //update for v0.5.5+
   module.exports = require('./build/Release/vips');
+} catch(e) {
+  //for < v0.5.5+
+  module.exports = require('./build/default/vips');
 }
